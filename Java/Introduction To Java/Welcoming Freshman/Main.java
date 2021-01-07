23b5a9842af118f381a125cb693bdfadd60446a6
Shubham Chaudhary <skchaudhari903@gmail.com>
2021-01-07 14:35:45 +0530

    Welcoming freshman

---
+++ Java/Introduction To Java/Welcoming Freshman/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name = scan.nextLine();
+        System.out.println("Welcome " +name + "! Achieve with all your might.");
+    }
+}
