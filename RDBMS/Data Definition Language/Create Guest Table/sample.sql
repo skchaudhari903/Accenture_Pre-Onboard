0f104f90ffe4ed326917c56d432943bc557a0f40
Shubham Chaudhary <skchaudhari903@gmail.com>
2021-01-28 18:08:55 +0530

    Create Guest table

---
+++ RDBMS/Data Definition Language/Create Guest Table/sample.sql
@@ -0,0 +1,9 @@
+Create table Guest(
+	guestID number,
+  	name varchar(30),
+    address varchar(30),
+  	country varchar(20),
+  	email varchar(30),
+  	phone varchar(15),
+  	constraint PK primary key(guestID)
+);
