f9d09c8431d3cb690f69898f4692e156539c36a5
Shubham Chaudhary <skchaudhari903@gmail.com>
2021-01-31 21:00:31 +0530

    Update data in Resort & Schedule table

---
+++ RDBMS/Data Manipulation Language/Update star-rating in Resort Table/sample.sql
@@ -0,0 +1,1 @@
+ update resort set starRating=4.2 where resortName='TREE OF LIFE RESORT';
---
+++ RDBMS/Data Manipulation Language/Update table Schedule/dml3.sql
@@ -0,0 +1,2 @@
+update schedule set travel_date = travel_date+5 
+where destination = 'Bangalore' and source = 'Chennai';
